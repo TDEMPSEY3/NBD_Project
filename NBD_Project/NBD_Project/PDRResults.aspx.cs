46a1da38570ac6ded181abd8be6d24e6c7e5916d
Torika Feaste <34326254+tfeaste1@users.noreply.github.com>
2017-12-12 23:37:11 -0500

    ResultsPage
    I created the result Page.

---
+++ NBD_Project/NBD_Project/PDRResults.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace NBD_Project
+{
+    public partial class Results : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //make sure user is logged into the system
+            if (!User.Identity.IsAuthenticated)
+                Response.Redirect("~/LogIn.aspx");
+            else
+                lblWelcome.Text += " " + User.Identity.Name;
+        }
+
+        protected void btnLogout_Click(object sender, EventArgs e)
+        {
+            var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+
+            authenticationManager.SignOut();
+
+            Response.Redirect("~/Login.aspx");
+        }
+    }
+}
